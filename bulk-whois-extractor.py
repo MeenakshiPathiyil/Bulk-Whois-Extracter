38274f1a5789a293ca50511fc1d2734b2c468e19
Meenu2019 <54184349+Meenu2019@users.noreply.github.com>
2023-07-19 19:58:26 +0530

    change file name

--- bulk-whois-extractor.py
+++
@@ -1,8 +0,0 @@
-import whois
-
-
-user_domain = input("Enter the domain name: ")
-user_parameter = input("Enter the parameter: ")
-p = whois.whois(user_domain)
-user_output = p[user_parameter]
-print(user_output)
